1b2bbbeb5e7aa2395bc69ce1877d88e568ede945
sonam02prajapati <126792126+sonam02prajapati@users.noreply.github.com>
2024-03-21 19:00:49 +0530

    Add files via upload

---
+++ Gsap.js
@@ -0,0 +1,19 @@
+tweenmax.from(".left-container", 2,{
+    width: "0",
+    ease: Expo.easeInout
+
+});
+tweenmax.from(".right-container", 2,{
+    delay: 1.5,
+    width: "100",
+    spacity: "0",
+    ease: Expo.easeInout
+
+});
+tweenmax.from(".center-container", 2,{
+    delay: 3,
+    width: "0",
+    x: -20,
+    ease: Expo.easeInout
+
+});
